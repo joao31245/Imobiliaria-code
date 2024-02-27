6e6905e100eb9356dff511944f0977f2dada6c90
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-02-27 10:47:01 -0300

    Create project

---
+++ src/main/java/com/jotace/imob/ImobApplication.java
@@ -0,0 +1,13 @@
+package com.jotace.imob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotace/imob/ImobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotace.imob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
