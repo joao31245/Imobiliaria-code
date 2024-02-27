6cad5302c345d75fbbd9d5a16a25f9d1197e05da
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-02-27 10:47:20 -0300

    Create user controller

---
+++ src/main/java/com/jotace/imob/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.jotace.imob.controller;
+
+import com.jotace.imob.dto.GetUserDTO;
+import com.jotace.imob.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<GetUserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+}
