a9368b110f93898801b3bf14df925ff4a125acd8
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-02-27 10:47:46 -0300

    Create get user DTO

---
+++ src/main/java/com/jotace/imob/dto/GetUserDTO.java
@@ -0,0 +1,18 @@
+package com.jotace.imob.dto;
+
+import com.jotace.imob.entity.user.User;
+import com.jotace.imob.entity.user.UserType;
+
+public record GetUserDTO(
+        Long id,
+        String name,
+        String email,
+        String phone,
+        UserType userType
+
+) {
+
+    public GetUserDTO(User user) {
+        this(user.getId(), user.getName(), user.getEmail(),user.getPhone(), user.getUserType());
+    }
+}
