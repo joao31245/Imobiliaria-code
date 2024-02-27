1eb4796bbbc1d4c784a6b404cfc1996c9f9639bb
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-02-27 13:06:02 -0300

    Create security filter

---
+++ src/main/java/com/jotace/imob/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.jotace.imob.infra.security;
+
+import com.jotace.imob.service.user.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+
+    private final TokenService tokenService;
+
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if(token != null) {
+            var login = this.tokenService.validateToken(token);
+
+            UserDetails user = userService.findUserByEmail(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ","");
+    }
+}
