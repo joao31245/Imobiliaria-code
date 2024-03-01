0ec676b257953f02b39eac5bf34cb66fcaa5016d
João Carlos Machado Filho <134329276+joao31245@users.noreply.github.com>
2024-03-01 13:23:41 -0300

    Create Post Service and PostType

---
+++ src/main/java/com/jotace/imob/entity/post/PostType.java
@@ -0,0 +1,8 @@
+package com.jotace.imob.entity.post;
+
+public enum PostType {
+    FOR_RENT,
+    FOR_SALE,
+    FOR_OVERNIGHT
+
+}
---
+++ src/main/java/com/jotace/imob/service/post/PostService.java
@@ -0,0 +1,86 @@
+package com.jotace.imob.service.post;
+
+import com.jotace.imob.dto.post.CreatePostRequestDTO;
+import com.jotace.imob.dto.post.CreatePostResponseDTO;
+import com.jotace.imob.dto.post.GetPostDTO;
+import com.jotace.imob.dto.post.InsertImageResponseDTO;
+import com.jotace.imob.entity.post.Post;
+import com.jotace.imob.repository.post.PostRepository;
+import com.jotace.imob.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    private final UserService userService;
+    public ResponseEntity<CreatePostResponseDTO> createPost(CreatePostRequestDTO createPostRequestDTO, UriComponentsBuilder uriBuilder) {
+        var post = new Post(createPostRequestDTO);
+
+        var user = userService.findById(createPostRequestDTO.userId());
+
+        post.setUser(user);
+
+        postRepository.save(post);
+
+        var uri = uriBuilder.path("/post/{id}").buildAndExpand(post.getId()).toUri();
+
+
+        return ResponseEntity.created(uri).body(new CreatePostResponseDTO(post));
+    }
+
+    public ResponseEntity<InsertImageResponseDTO> insertImage(MultipartFile file, Long id) {
+        try {
+            byte[] fileBytes = file.getBytes();
+
+            Blob blob = new SerialBlob(fileBytes);
+
+            var post = postRepository.findPostById(id);
+
+            post.setImage(blob);
+
+            postRepository.save(post);
+
+            return ResponseEntity.ok().build();
+        } catch (IOException | SQLException ex) {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    public ResponseEntity<List<GetPostDTO>> getAllPosts() {
+        try {
+            var posts = postRepository.findAll();
+            List<GetPostDTO> dtos = posts.stream()
+                    .map(post -> {
+                        try {
+                            return new GetPostDTO(post);
+                        } catch (SQLException | IOException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(dtos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
